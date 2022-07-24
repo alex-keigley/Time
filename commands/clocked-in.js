5218dbcd9176ebc5f0a8654ed5089fbf4925513e
akeigley-jSFptj <akeigley@instructors.2u.com>
2022-07-24 01:53:08 -0500

    fixed Promises, now works as intended

--- commands/clocked-in.js
+++ commands/clocked-in.js
@@ -20,10 +20,51 @@ module.exports = {
             return(m)
         }
 
-        console.log(await getIDs())
-
         // get member info
+        async function getMembers(id_list) {
+            membersClockedIn = []
+            await id_list.forEach((id => {
+                Member.findOne({ ds_id: id }, (err, member) => {
 
-        // create embed
+                    // Calculate time since clock-in
+                    shift_length = new Date().getTime() - member.current_shift.start_time
+
+                    membersClockedIn.push({
+                        name: member.ds_nick,
+                        shift_length: shift_length
+                    })
+                })
+            }))
+        
+            return new Promise((resolve) => {
+                setTimeout(() => resolve(membersClockedIn), 300)
+            })
+        }
+
+        const clockedInMembers = await getMembers(await getIDs())
+
+        if (clockedInMembers.length === 0) {
+            embed = new EmbedBuilder()
+                .setColor('#1E90FF')
+                .setTitle('Clocked in:')
+                .setDescription('There is nobody clocked-in.')
+            interaction.reply({embeds: [embed] })
+        }
+        else {
+            // create description text for embed
+            let message = ''
+            clockedInMembers.forEach((member => {
+                shift_length = new Date(member.shift_length)
+                shift_length = shift_length.toISOString().slice(11, 19); // 👉️ 15:00:00
+                message = message.concat(`**${member.name}** \n *${shift_length}* \n`)
+            }))
+
+            // create and send embed
+            embed = new EmbedBuilder()
+                .setColor('#1E90FF')
+                .setTitle('Clocked in:')
+                .setDescription(message)                
+            interaction.reply({embeds: [embed]})
+        }
     },
 }
